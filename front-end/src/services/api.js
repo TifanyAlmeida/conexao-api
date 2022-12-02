38e86c5f2008ccb5a7696a5d24ccc5f283a470da
Tifany Almeida dos Santos <53823428810>
2022-12-02 09:40:33 -0300

    logica pronta mas nn funcionas

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiLogin = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "http://127.0.0.1:8000/api/register",
 });
 
 export default apiLogin;
